4bf299bd4d493b25dbbd9ecfc07c29033f7c549e
yadsun <yadongsun1984@gmail.com>
2015-10-16 14:19:45 +0800

    Update native2ascii.scala

--- native2ascii.scala
+++ native2ascii.scala
@@ -1,1 +1,12 @@
-
+def native2Ascii(chars:CharSequence):CharSequence={
+  
+}
+def char2Ascii(char:Char):CharSequence={
+  
+}
+def ascii2Native(asciis:CharSequence):CharSequence={
+  
+}
+def ascii2Char(ascii:CharSequence):Char={
+  
+}
